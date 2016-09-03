4ad69f27a7eac4bfe78c29e4a6f0b436feca803e
hourunjing <hourunjing@hourunjingdeMacBook-Pro.local>
2016-09-03 13:50:58 +0800

    初始化以及添加LeanCloud支持和YYText库

---
+++ AnswerMemo/AnswerMemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnswerMemo
+//
+//  Created by hourunjing on 2016/9/3.
+//  Copyright © 2016年 com.hbzsios.answermemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnswerMemo/AnswerMemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnswerMemo
+//
+//  Created by hourunjing on 2016/9/3.
+//  Copyright © 2016年 com.hbzsios.answermemo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
